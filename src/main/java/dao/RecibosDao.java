0b78d000534a4fd4f4501f8d5938d9b58508b023
GiancarloCordova <gcordova-2024016@kinal.edu.gt>
2025-07-28 18:16:06 -0600

    Se configuro el DAO y Pojo de Recibos

--- src/main/java/dao/RecibosDao.java
+++ src/main/java/dao/RecibosDao.java
@@ -1,8 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import model.RecibosPojo;
 
 /**
  *
@@ -10,4 +12,62 @@ package dao;
  */
 public class RecibosDao {
     
+    private EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("funkinalPU");
+    
+    public void guardar(RecibosPojo recibo) {
+        EntityManager admin = fabrica.createEntityManager();
+        EntityTransaction transaccion = admin.getTransaction();
+        try {
+            transaccion.begin();
+            admin.persist(recibo);
+            transaccion.commit();
+        } catch (Exception e) {
+            if (transaccion.isActive()) transaccion.rollback();
+            e.printStackTrace();
+        }finally {
+            admin.close();
+        }
+    }
+    
+    public RecibosPojo buscarPorId(int id) {
+        EntityManager admin = fabrica.createEntityManager();
+        try {
+            return admin.find(RecibosPojo.class, id);
+        } finally {
+            admin.close();
+        }
+    }
+
+    public void Actualizar(RecibosPojo recibo) {
+        EntityManager admin = fabrica.createEntityManager();
+        EntityTransaction transaccion = admin.getTransaction();
+        try {
+            transaccion.begin();
+            admin.merge(recibo);
+            transaccion.commit();
+        } catch (Exception e) {
+            if (transaccion.isActive()) transaccion.rollback();
+            e.printStackTrace();
+        } finally {
+            admin.close();
+        }
+    }
+
+    public void Eliminar(int id) {
+        EntityManager admin = fabrica.createEntityManager();
+        EntityTransaction transaccion = admin.getTransaction();
+        try {
+            transaccion.begin();
+            RecibosPojo recibo = admin.find(RecibosPojo.class, id);
+            if (recibo != null) {
+                admin.remove(recibo);
+            }
+            transaccion.commit();
+        } catch (Exception e) {
+            if (transaccion.isActive()) transaccion.rollback();
+            e.printStackTrace();
+        } finally {
+            admin.close();
+        }
+    }
 }
--- src/main/java/model/RecibosPojo.java
+++ src/main/java/model/RecibosPojo.java
@@ -1,13 +1,93 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author acord
- */
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Recibos")
 public class RecibosPojo {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idRecibo;
+
+    @Column(name = "idCompra", nullable = false)
+    private int idCompra;
+
+    @Column(name = "fechaEmision", nullable = false)
+    private Timestamp fechaEmision;
+
+    @Column(name = "total", nullable = false)
+    private BigDecimal total;
+
+    @Column(name = "metodoPago", nullable = false)
+    private String metodoPago;
+
+    @Column(name = "estado")
+    private String estado;
+
+    public RecibosPojo() {
+    }
+
+    public RecibosPojo(int idCompra, Timestamp fechaEmision, BigDecimal total, String metodoPago, String estado) {
+        this.idCompra = idCompra;
+        this.fechaEmision = fechaEmision;
+        this.total = total;
+        this.metodoPago = metodoPago;
+        this.estado = estado;
+    }
+
+    public int getIdRecibo() {
+        return idRecibo;
+    }
+
+    public void setIdRecibo(int idRecibo) {
+        this.idRecibo = idRecibo;
+    }
+
+    public int getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(int idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Timestamp getFechaEmision() {
+        return fechaEmision;
+    }
+
+    public void setFechaEmision(Timestamp fechaEmision) {
+        this.fechaEmision = fechaEmision;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
