f39d692f59bb787c14e19f3e58180a6273c03ccf
Informatica <informatica@alumno.local>
2025-07-29 11:56:25 -0600

    Se modifico el archivo UsuariosDao.java

---
+++ src/main/java/dao/UsuariosDao.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import model.UsuariosPojo;
+
+/**
+ *
+ * @author informatica
+ */
+public class UsuariosDao {
+    
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("funkinalPU");
+    
+    public void agregar(UsuariosPojo usuario){
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaccion = em.getTransaction();
+        
+        try {
+            transaccion.begin();
+            em.persist(usuario);
+            transaccion.commit();
+        } catch (Exception e) {
+            if (transaccion.isActive()) {
+                transaccion.rollback();
+            }
+            System.err.println("Error al agregar usuario: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+    
+    public List<UsuariosPojo> listarUsuarios(){
+        String jpql = "SELECT u FROM Usuarios u";
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.createQuery(jpql, UsuariosPojo.class).getResultList();
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            em.close();
+        }
+        return new ArrayList<>();
+    }
+    
+    public UsuariosPojo buscarPorId(int id){
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.find(UsuariosPojo.class, id);
+        } finally {
+            em.close();
+        }
+    }
+    
+    public void actualizar(UsuariosPojo usuario){
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaccion = em.getTransaction();
+        try {
+            transaccion.begin();
+            em.persist(usuario);
+            transaccion.commit();
+        } catch (Exception e) {
+            if (transaccion.isActive()) transaccion.rollback();
+        }finally {
+            em.close();
+        }
+    }
+    
+    public void eliminar(int id){
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaccion = em.getTransaction();
+        try {
+            transaccion.begin();
+            UsuariosPojo usuarios = em.find(UsuariosPojo.class, id);
+            if (usuarios != null){
+                em.remove(usuarios);
+            }
+            transaccion.commit();
+        } catch (Exception e) {
+            if (transaccion.isActive()) transaccion.rollback();
+        }finally {
+            em.close();
+        }          
+    }
+}
